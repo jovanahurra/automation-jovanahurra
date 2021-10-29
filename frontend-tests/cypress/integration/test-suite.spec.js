0e1d22d312df0fb6ce2a8f988e54cdabee5119e9
jovanahurra <jovanahurra@gmail.com>
2021-10-29 11:21:00 +0200

    Created login-logout test case

--- frontend-tests/cypress/integration/test-suite.spec.js
+++ frontend-tests/cypress/integration/test-suite.spec.js
@@ -6,6 +6,13 @@ describe('Test suite', function(){
     //This is a test case
     it('Perform valid loign', function(){
         cy.visit('http://localhost:3000')
-
+        cy.contains('Login')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.get('.user > .btn').click()
+        cy.contains('Login')
+                
     })
 })
